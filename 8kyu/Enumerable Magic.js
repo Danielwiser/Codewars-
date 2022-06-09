db2ee6ec778555d89fbd5bc04bb6669437bf1565
Danielwiser <danielwiser1111@gmail.com>
2022-06-09 16:44:14 +0000

    new commit

--- 8kyu/Enumerable Magic.js
+++ 8kyu/Enumerable Magic.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
 // each_cons([1,2,3,4], 2)
 //   #=> [[1,2], [2,3], [3,4]]
@@ -28,4 +29,36 @@ function eachCons(array, n) {
     
     return res.filter(e => e.length === n);
   }
+=======
+// Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+// each_cons([1,2,3,4], 2)
+//   #=> [[1,2], [2,3], [3,4]]
+
+//   each_cons([1,2,3,4], 3)
+//     #=> [[1,2,3],[2,3,4]]
+
+// P   objects
+// R   
+// E   each_cons([1,2,3,4], 2)
+//   #=> [[1,2], [2,3], [3,4]]
+
+//   each_cons([1,2,3,4], 3)
+//     #=> [[1,2,3],[2,3,4]]
+    
+//   As you can see, the lists are cascading; ie, they overlap, but never out of order.
+
+// P   create an object 
+
+
+//solution
+function eachCons(array, n) {
+    let res = [];
+    
+    for(let i = 0; i < array.length; i++){
+      res.push(array.slice(i,i + n));
+    }
+    
+    return res.filter(e => e.length === n);
+  }
+>>>>>>> 060223f (new commit)
   
