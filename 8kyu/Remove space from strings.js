5faa6d071e7bb5bda20f7aaecfd93dcf1cf18e90
Danielwiser <danielwiser1111@gmail.com>
2022-06-11 22:26:52 +0000

    added commit

---
+++ 8kyu/Remove space from strings.js
@@ -0,0 +1,15 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+// P    Removing spaces between strings
+// R   
+// E    so simple
+// P    
+
+
+
+// solution
+
+const noSpace = (x) => {
+    return x.replace(/\s/g, '')
+}
